70b22c63747f45c0aaf0263bdb2d184fe0804b18
shubh-codes7 <shubhamcodes7@gmail.com>
2025-03-22 10:56:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 function firstWord(s) {
   // your code here
+	let match = s.trim().match(/^\S+/)
+	return match ? match[0] : "";
 }
 
 // Do not change the code below
 
-const s = prompt("Enter String:");
+const s = prompt("Enter String:"); 
 alert(firstWord(s));
